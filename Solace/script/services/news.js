655dbfb9bebdc65343f1804cd092f6f443722a67
Tiia Pitkänen <105545170+T-Pitkanen@users.noreply.github.com>
2023-05-28 14:03:31 +0200

    Add files via upload

---
+++ Solace/script/services/news.js
@@ -0,0 +1,28 @@
+export function getNews() {
+	async function getNewsData() {
+		const res = await fetch(
+			'https://newsapi.org/v2/top-headlines?country=us&apiKey=869c7910a9794249a9906f0ac4fc2fc0'
+		);
+		const data = await res.json();
+		console.log(data);
+		showNewsData(data);
+	}
+
+	getNewsData();
+
+	//Display News Data
+	function showNewsData(data, index = 0) {
+		const newsEl = document.querySelector('#news');
+		const newsData = data.articles[index].title;
+
+		newsEl.innerHTML = newsData;
+
+		console.log(newsData);
+
+		//Loop through news data every 10 seconds
+		setTimeout(() => {
+			const newIndex = (index + 1) % data.articles.length;
+			showNewsData(data, newIndex);
+		}, 10000);
+	}
+}
