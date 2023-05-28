c903966096b2f1620110aa4fd6b471e74196dc2d
Tiia Pitkänen <105545170+T-Pitkanen@users.noreply.github.com>
2023-05-28 14:04:06 +0200

    Delete Solace directory

--- Solace/script/index.js
+++
@@ -1,9 +0,0 @@
-import { getTimeAndDate } from './common/time.js';
-import { getData } from './services/data.js';
-import { getWeather } from './services/weather.js';
-import { getNews } from './services/news.js';
-
-getTimeAndDate();
-getData();
-getWeather();
-getNews();
