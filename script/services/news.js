bd1c9ae97478bfa030e9979025510b92c541d106
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2024-04-16 18:24:16 +0200

    fixes

--- script/services/news.js
+++ script/services/news.js
@@ -1,28 +1,32 @@
 export function getNews() {
-	async function getNewsData() {
-		const res = await fetch(
-			'https://newsapi.org/v2/top-headlines?country=us&apiKey=869c7910a9794249a9906f0ac4fc2fc0'
-		);
-		const data = await res.json();
-		console.log(data);
-		showNewsData(data);
-	}
+  async function getNewsData() {
+    const res = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=869c7910a9794249a9906f0ac4fc2fc0"
+    );
+    const data = await res.json();
+    console.log(data);
+    showNewsData(data);
+  }
 
-	getNewsData();
+  getNewsData();
 
-	//Display News Data
-	function showNewsData(data, index = 0) {
-		const newsEl = document.querySelector('#news');
-		const newsData = data.articles[index].title;
+  //Display News Data
+  function showNewsData(data, index = 0) {
+    const newsEl = document.querySelector("#news");
+    let newsData = data.articles[index].title;
 
-		newsEl.innerHTML = newsData;
+    if (!newsData) {
+      newsData = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    }
 
-		console.log(newsData);
+    newsEl.innerHTML = newsData;
 
-		//Loop through news data every 10 seconds
-		setTimeout(() => {
-			const newIndex = (index + 1) % data.articles.length;
-			showNewsData(data, newIndex);
-		}, 15000);
-	}
+    console.log(newsData);
+
+    //Loop through news data every 10 seconds
+    setTimeout(() => {
+      const newIndex = (index + 1) % data.articles.length;
+      showNewsData(data, newIndex);
+    }, 15000);
+  }
 }
